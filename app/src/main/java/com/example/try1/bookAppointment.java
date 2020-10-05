a4c4cda75dc1a01d25645b8336f13535879f24db
anukulmuley2000@gmail.com <Anukul123@2000>
2020-10-05 22:56:08 +0530

    inspire healthcare1 first commit

---
+++ app/src/main/java/com/example/try1/bookAppointment.java
@@ -0,0 +1,16 @@
+package com.example.try1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bookAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle
+                                        savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_appointment);
+        getSupportActionBar().hide();
+    }
+}
---
+++ app/src/main/java/com/example/try1/caseHistory.java
@@ -0,0 +1,15 @@
+package com.example.try1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class caseHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_case_history);
+        getSupportActionBar().hide();
+    }
+}
