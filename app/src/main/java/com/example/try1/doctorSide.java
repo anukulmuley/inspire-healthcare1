63d6b52496e5cb9ebc10a00bbf0ad94720db95ce
anukulmuley2000@gmail.com <Anukul123@2000>
2020-10-06 00:55:21 +0530

    inspire healthcare1 first commit

--- app/src/main/java/com/example/try1/doctorSide.java
+++ app/src/main/java/com/example/try1/doctorSide.java
@@ -55,13 +55,17 @@ public class doctorSide extends AppCompatActivity implements View.OnClickListene
                 startActivity(i);
                 break;
             case R.id.d5:
-                i=new Intent (this,meetings.class);
-                startActivity(i);
+//                i=new Intent (this,meetings.class);
+////                startActivity(i);
+////                break;
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://meet.google.com/"));
+                startActivity(browserIntent);
                 break;
+
             case R.id.d6:
 //                i=new Intent (this,medicalOrder.class);
 //                startActivity(i);
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.industrybuying.com/"));
+                browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.industrybuying.com/"));
                 startActivity(browserIntent);
                 break;
 
