00d40529f31d379a3265d80b7aa767f68637569d
anukulmuley2000@gmail.com <Anukul123@2000>
2020-10-07 01:47:20 +0530

    inspire healthcare1 first commit

---
+++ app/src/main/java/com/example/try1/uploadLabReports.java
@@ -0,0 +1,14 @@
+package com.example.try1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class uploadLabReports extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_lab_reports);
+    }
+}
